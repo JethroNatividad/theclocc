8a717b5bf1c9f673db20f1a4fbd5d210649442bd
JethroSama <kel73896@gmail.com>
2021-12-14 23:35:41 +0800

    Add search functionality

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,21 +2,35 @@ import React, { useState } from 'react'
 import './Sidebar.css'
 import searchIcon from './icons/search.svg'
 const Sidebar = ({ changeTimezone, timezone, timezoneList }) => {
-    const [results, setResults] = useState([{ query: "Africa/Abidjan", text: 'Abidjan' }])
+    const [results, setResults] = useState([])
+    const [inputValue, setInputValue] = useState('')
     return (
         <div className="Main-sidebar">
             <div className="searchbar">
-                <input className="search-input" type="text" placeholder="Another timezone" />
+                <input onChange={(e) => {
+                    // check if empty, set results to nothing
+                    if (e.target.value === '') {
+                        setResults([])
+                    } else {
+                        const results = timezoneList.filter(timezone => timezone.query.toLowerCase().includes(e.target.value.toLowerCase()))
+                        setResults(results)
+                    }
+                    setInputValue(e.target.value)
+                }} className="search-input" type="text" placeholder="Another timezone" />
                 <div className="search-button">
                     <img src={searchIcon} alt="search" className="search-icon" />
                 </div>
             </div>
             <div className="bottom-sidebar">
-
-                <div className="search-results">
-                    {results.map(result => (<p className={`timezone-item ${timezone === result.query && 'timezone-item-active'}`} onClick={() => changeTimezone(result.query)}>{result.text}</p>))}
-                </div>
-                <div className="divider" />
+                {results.length > 0 && inputValue.length > 0 ? <>
+                    <div className="search-results">
+                        {results.map(result => (<p className={`timezone-item ${timezone === result.query && 'timezone-item-active'}`} onClick={() => changeTimezone(result.query)}>{result.text}</p>))}
+                    </div>
+                    <div className="divider" />
+                </> : inputValue.length > 0 ? <>
+                    <p className="not-found">Not found</p>
+                    <div className="divider" />
+                </> : null}
                 <div className="timezones">
                     <h2>Timezones</h2>
                     {timezoneList.map(result => (<p className={`timezone-item ${timezone === result.query && 'timezone-item-active'}`} onClick={() => changeTimezone(result.query)} >{result.text}</p>))}
