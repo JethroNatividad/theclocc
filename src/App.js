d8ee7e433c940cfd5d6012d499cb440b51763c2f
JethroSama <kel73896@gmail.com>
2021-12-14 19:25:13 +0800

    Style main

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      hello world
+      <div className="Main">
+        <div className="Main-title">the.CLOCC</div>
+        <div className="Main-clock">
+          <p className="time">12:00:00</p>
+          <div className="timezone-container">
+            <p className="timezone">Asia/Manila</p>
+            <p>+08:00 - Sunday, 6 Dec 10</p>
+          </div>
+        </div>
+      </div>
+      <div className="Sidebar">
+        sidebar
+      </div>
     </div>
   );
 }
