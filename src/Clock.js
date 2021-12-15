39303f1928a98d7b7a8f00852cdfb5bc48763a00
JethroSama <kel73896@gmail.com>
2021-12-15 18:12:23 +0800

    set dynamic date

--- src/Clock.js
+++ src/Clock.js
@@ -9,7 +9,8 @@ const Clock = ({ timezone, datetime, utc_offset }) => {
             <p className="time">{moment.tz(datetime, timezone).format('HH:mm:ss')}</p>
             <div className="timezone-container">
                 <p className="timezone">{timezone}</p>
-                <p>{utc_offset} - Sunday, 6 Dec 10</p>
+                <p>{utc_offset} - {moment.tz(datetime, timezone).format('dddd, MMMM D YYYY')}</p>
+
             </div>
         </div>
     )
